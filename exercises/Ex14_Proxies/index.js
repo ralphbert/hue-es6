9c9d225d1a1276d4408c6023498ba55aadcaaf7e
Ralph Harrer <ralph.harrer@cyberhouse.at>
2017-03-05 13:59:07 +0100

    added extended example

--- exercises/Ex14_Proxies/index.js
+++ exercises/Ex14_Proxies/index.js
@@ -1,5 +1,27 @@
 var target = function () {
-    return 'You killed my father!';
+  return 'You killed my father!';
+};
+console.log(target());
+
+/*
+ let spoilerProxy = ...;
+ // TODO: create a proxy called addSpoiler which adds the Spoiler "No, I am your father" at the end of the string.
+ console.log(spoilerProxy());
+ */
+
+var object = {};
+var loggingHandler = {};
+// TODO: add logging functions to set and get operations. print out the property name, the value und the operation (get, set)
+
+var loggedObject = new Proxy(object, loggingHandler);
+
+loggedObject.a = 'this is a';
+loggedObject.b = 100;
+loggedObject.c = function () {
+  return 'this is c';
 };
 
-// TODO: create a proxy called addSpoiler which adds the Spoiler "No, I am your father" at the end of the string.
+console.log(loggedObject.a);
+console.log(loggedObject.b);
+console.log(loggedObject.c);
+console.log(loggedObject.c());
